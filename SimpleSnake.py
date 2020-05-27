a6d81eb6c7c7dffc03a35fbdc886dca0d91b0177
Will Ludwig <williampludwig@gmail.com>
2020-05-27 12:47:56 -0600

    Game is working! Fixed some indentation errors

--- SimpleSnake.py
+++ SimpleSnake.py
@@ -20,8 +20,8 @@ snake = [
     [y_coordinate, x_coordinate-2]
 ]
 
-goal = [screenheight/2, screenwidth/4]
-options.addch(int(goal[0]), int(goal[1]), curses.ACS_PI)
+goal = [screenheight/2, screenwidth/2]
+options.addch(int(goal[0]), int(goal[1]), curses.ACS_DIAMOND)
 
 control = curses.KEY_RIGHT
 
@@ -34,33 +34,33 @@ while True:
         curses.endwin()
         quit()
 
-        refresh_head = [snake[0][0], snake[0][1]]
+    refresh_head = [snake[0][0], snake[0][1]]
 
-        # Check user input and move the snake
-        if control == curses.KEY_DOWN:
-            refresh_head[0] += 1
-        if control == curses.KEY_UP:
-            refresh_head[0] -= 1
-        if control == curses.KEY_RIGHT:
-            refresh_head[1] += 1
-        if control == curses.KEY_LEFT:
-            refresh_head[1] -= 1
+    # Check user input and move the snake
+    if control == curses.KEY_DOWN:
+        refresh_head[0] += 1
+    if control == curses.KEY_UP:
+        refresh_head[0] -= 1
+    if control == curses.KEY_RIGHT:
+        refresh_head[1] += 1
+    if control == curses.KEY_LEFT:
+        refresh_head[1] -= 1
 
-        snake.insert(0, refresh_head)
+    snake.insert(0, refresh_head)
 
-        # Check if snake has eaten the goal
-        if snake[0] == goal:
-            goal = None
-            while goal is None:
-                # Create new goal
-                newGoal = [
-                    random.randint(1, screenheight-1),
-                    random.randint(1, screenwidth-1)
-                ]
-                goal = newGoal if newGoal not in snake else None
-            options.addch(goal[0], goal[1], curses.ACS_PI)
-        else:
-            tail = snake.pop()
-            options.addch(int(tail[0]), int(tail[1]), ' ')
+    # Check if snake has eaten the goal
+    if snake[0] == goal:
+        goal = None
+        while goal is None:
+            # Create new goal
+            newGoal = [
+                random.randint(1, screenheight-1),
+                random.randint(1, screenwidth-1)
+            ]
+            goal = newGoal if newGoal not in snake else None
+        options.addch(goal[0], goal[1], curses.ACS_DIAMOND)
+    else:
+        tail = snake.pop()
+        options.addch(int(tail[0]), int(tail[1]), ' ')
 
-        options.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+    options.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
